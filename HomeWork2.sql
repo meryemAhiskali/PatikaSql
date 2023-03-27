19ce271645407d0d9a972b615c26064116294617
meryemAhiskali <120027848+meryemAhiskali@users.noreply.github.com>
2023-03-28 00:31:43 +0300

    Update HomeWork2.sql

--- HomeWork2.sql
+++ HomeWork2.sql
@@ -3,7 +3,6 @@ select * from film
 where replacement_cost >= 12.99 AND replacement_cost<16.99;
 
 --actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
-
 select first_name, last_name from actor
 where first_name IN ('Penelope','Nick','Ed');
 
