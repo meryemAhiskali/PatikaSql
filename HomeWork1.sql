59f0e9af82a40704a1d408746687c4ef8c429723
meryemAhiskali <120027848+meryemAhiskali@users.noreply.github.com>
2023-03-28 00:04:02 +0300

    Update HomeWork1.sql

--- HomeWork1.sql
+++ HomeWork1.sql
@@ -8,7 +8,7 @@ where LENGTH > 60 AND LENGTH <75;
 
 -- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 select * from film 
-where RENTAL_RATE = 0.99 AND replacement_Cost=12.99 OR replacement_cost =28.99;
+where RENTAL_RATE = 0.99 AND replacement_cost=12.99 OR replacement_cost =28.99;
 
 -- customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 select last_name from customer 
