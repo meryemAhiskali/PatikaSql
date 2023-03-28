b194e78f8f441a97e0f71f40119e107d86de574b
meryemAhiskali <120027848+meryemAhiskali@users.noreply.github.com>
2023-03-28 09:55:41 +0300

    Create Homework3.sql

---
+++ Homework3.sql
@@ -0,0 +1,19 @@
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
+select * from country
+where country LIKE 'A%a';
+
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+select * from country 
+where LENGTH(country)>= 6 AND country LIKE '%n';
+
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+select title from film 
+where title ILIKE '%t%t%t%t%';
+
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+select * from film
+where title LIKE 'C%' AND LENGTH(title)>90 AND rental_rate = 2.99;
